ab9aa60d8aff5b4b9def79ed1c52fd6fafa86d15
AlexM <example@gmail.com>
2023-08-27 10:29:29 +0300

    add loops

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
-'use strict'
+'use strict';
 
 const numberOfFilms = +prompt('How much have you already wachted films?', '');
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
 };
+let lastMovie;
+let rating;
+let count = numberOfFilms;
+for( let i = 0; i < count; i++) {
+	lastMovie = prompt('What have you watched last film?', '');
+	rating = prompt('What do you evaluate this film?', '');
+	if(lastMovie === '' || lastMovie.length >= 50 || lastMovie === null){
+		count++;	
+	}
+	else{
+		personalMovieDB.movies[lastMovie] = rating;
+		console.log(`last film is ${lastMovie} has rating - ${personalMovieDB.movies[lastMovie]}`);
+	}
+}
 
-const lastMovie = prompt('What have you watched last film?', '');
-const rating = prompt('What do you evaluate this film?', '');
-const lastMovie1 = prompt('What have you watched last film?', '');
-const rating1 = prompt('What do you evaluate this film?', '');
+// do while /////////////////////////////////////////////////
 
-personalMovieDB.movies[lastMovie] = rating;
-personalMovieDB.movies[lastMovie1] = rating1;
+// let j = 0;
+// do{
+// 	lastMovie = prompt('What have you watched last film?', '');
+// 	rating = prompt('What do you evaluate this film?', '');
+// 	if(lastMovie === '' || lastMovie.length >= 10 ){
+// 		count++;	
+// 	}
+// 	else{
+// 		personalMovieDB.movies[lastMovie] = rating;
+// 		console.log(`last film is ${lastMovie} has rating - ${personalMovieDB.movies[lastMovie]}`);
+// 	}
+// 	j++;
+// } while (j < count);
 
-console.log(`last film is ${lastMovie} has rating - ${personalMovieDB.movies[lastMovie]}`);
+// first answer /////////////////////////////////////////////////////////
+//const lastMovie = prompt('What have you watched last film?', '');     //
+//const rating = prompt('What do you evaluate this film?', '');         //
+//const lastMovie1 = prompt('What have you watched last film?', '');    //
+//const rating1 = prompt('What do you evaluate this film?', '');
+
+//personalMovieDB.movies[lastMovie] = rating;
+//personalMovieDB.movies[lastMovie1] = rating1;
+//////////////////////////////////////////////////////////////////////////
+
+
+
